f1af258994ee735d9aa6538acb83526c8255e728
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-09 14:22:09 +0530

    [Ganesh] Add:Vending mchine is added

--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -6,6 +6,24 @@ namespace NUnitTesting
         static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Nunit testing!");
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("1.Vending machine\n2.exit\nEnter your option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        VendingManchine vending = new VendingManchine();
+                        Console.WriteLine("Enter amount");
+                        int amount = Convert.ToInt32(Console.ReadLine());
+                        vending.giveChange(amount);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
---
+++ NUnitTesting/VendingManchine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    public class VendingManchine
+    {
+        int[] notes = { 2000, 500, 100, 50, 20, 5, 2, 1 };  
+        public void giveChange(int amount)
+        {
+            int countOfNotes = 0;
+            for(int i = 0; i < notes.Length; i++)
+            {
+                if (amount > 0 && amount / notes[i] >= 1)
+                {
+                    int change = amount / notes[i];
+                    countOfNotes += change;
+                    Console.WriteLine(notes[i] + "------>>>" + change);
+                    amount -= notes[i] * change;
+                }
+            }
+            Console.WriteLine("The total number of notes needed are " + countOfNotes);
+        }
+    }
+}
