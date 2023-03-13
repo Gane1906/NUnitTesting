44bb865eac95555274ed54e8a82aca8bdaf29f48
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-13 08:57:24 +0530

    [Ganesh] Add:Binary number is added

--- NUnitTesting/BinaryNumber.cs
+++ NUnitTesting/BinaryNumber.cs
@@ -10,30 +10,16 @@ namespace NUnitTesting
     {
         public static void toBinary(int num)
         {
-            int[] binary = new int[num];
-            int i = 0;
+            string binary = " ", res = " ";
             while (num > 0)
             {
-                binary[i] = num % 2;
-                i++;
+                binary += num % 2;
+                //i++;
                 num /= 2;
             }
-            Console.Write("The binary value of " + num + " is: ");
-            for (int j = i - 1; j >= 0; j--)
-                Console.Write(binary[j]);
-            Console.WriteLine();
-
+            for (int i = binary.Length - 1; i >= 0; i--)
+                res += binary[i];
+            Console.WriteLine(res);
         }
     }
 }
-/*
- * 10 >0
- * A[0]----- >0
- * 5 >0
- * A[1]----->1
- * 2 >0
- * A[2]----->0
- * 1>0
- *A[3]------>1 
- *
-*/
